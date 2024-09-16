3c0d0ae63f836b011570db0564cce76b6e7ab197
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-09-17 02:01:29 +0530

    update a little change in contact.jsx

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Contact.css'
 import Swal from 'sweetalert2'
-
+  
 // export default function Contact() {
 //     return (
 //         <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
